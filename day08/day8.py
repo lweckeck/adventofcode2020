ec03eec9de541c4714f7ef373d2a6c38cbf8fad1
Lennart Weckeck <lennart.weckeck@mailbox.org>
2020-12-08 22:47:55 +0100

    Refactoring.

--- day08/day8.py
+++ day08/day8.py
@@ -1,24 +1,22 @@
 import itertools
 import re
 from collections import defaultdict, deque
-from dataclasses import dataclass
-from typing import Dict, Iterable, List, Literal, Optional, Set, Tuple
+from typing import Dict, Iterable, List, NamedTuple, Set
 
 instruction_pattern = re.compile('^(acc|jmp|nop) ([+-][0-9]+)$')
-@dataclass
-class Instruction:
+class Instruction(NamedTuple):
     op: str
     arg: int
 
 def parse(lines: Iterable[str]) -> List[Instruction]:
     return [Instruction(match.group(1), int(match.group(2))) for line in lines if (match := instruction_pattern.match(line)) is not None]
 
-@dataclass
-class State:
+class State(NamedTuple):
     pc: int
     acc: int
 
 def run(program: List[Instruction]) -> Iterable[State]:
+    visited: Set[int] = set()
     yield (state := State(0, 0))
     while 0 <= state.pc and state.pc < len(program):
         instruction = program[state.pc]
@@ -28,43 +26,35 @@ def run(program: List[Instruction]) -> Iterable[State]:
             state = State(state.pc + instruction.arg, state.acc)
         else:
             state = State(state.pc + 1, state.acc)
-        yield state
-
-def noloop(trace: Iterable[State]) -> Iterable[State]:
-    visited: Set[int] = set()
-    for state in trace:
-        if state.pc in visited:
+        if state.pc in visited: # loop detected
             break
-        visited.add(state.pc)
         yield state
+        visited.add(state.pc)
 
 def find_terminating_indices(program: List[Instruction]) -> Set[int]:
     next_instruction = [ i + instruction.arg if instruction.op == 'jmp' else i + 1 for (i, instruction) in enumerate(program)] # execution graph
-    previous_instructions = defaultdict(set) # inverse execution graph
+    previous_instructions: Dict[int, Set[int]] = defaultdict(set) # inverse execution graph
     for (i, j) in enumerate(next_instruction):
         previous_instructions[j].add(i)
-    queue = deque([len(program)]) # last instruction + 1
-    safe: Set[int] = set()
-    while len(queue) > 0:
-        node = queue.popleft()
-        queue.extend(previous_instructions[node])
-        safe.add(node)
-    return safe
+    return set(reachable_from(len(program), previous_instructions))
+
+def reachable_from(start: int, descendants: Dict[int, Set[int]]):
+    return itertools.chain([start], *map(lambda desc: reachable_from(desc, descendants), descendants[start]))    
 
 if __name__ == "__main__":
     with open('day08/input') as f:
         program = parse(f)
 
-        loop_trace = list(noloop(run(program)))
+        loop_trace = list(run(program))
         print(f'Part 1: {loop_trace[-1].acc}')
 
-        terminating_indices = find_terminating_indices(program)
+        safe = find_terminating_indices(program)
         for state in loop_trace:
             instruction = program[state.pc]
-            if (instruction.op == 'jmp') and (state.pc + 1) in terminating_indices:
-                instruction.op = 'nop'
+            if (instruction.op == 'jmp') and (state.pc + 1) in safe:
+                program[state.pc] = Instruction('nop', instruction.arg)
                 break
-            if (instruction.op == 'nop') and (state.pc + instruction.arg) in terminating_indices:
-                instruction.op = 'jmp'
+            if (instruction.op == 'nop') and (state.pc + instruction.arg) in safe:
+                program[state.pc] = Instruction('jmp', instruction.arg)
                 break
         print(f'Part 2: {list(run(program))[-1].acc}')
