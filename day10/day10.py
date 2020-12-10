864886e0b01b9385be1d8f53a3433d019af0d2f1
Lennart Weckeck <lennart.weckeck@mailbox.org>
2020-12-10 19:22:19 +0100

    Refactor for memory efficiency.

--- day10/day10.py
+++ day10/day10.py
@@ -1,23 +1,18 @@
-from typing import Counter, List, Mapping
-
-def distribution(adapters: List[int]) -> Mapping[int, int]:
-    return Counter(adapters[i] - adapters[i-1] for i in range(1, len(adapters)))
+from collections import deque
+from typing import Counter, List
 
 def count_arrangements(adapters: List[int]) -> int:
-    connections = [1]
+    connections = deque([1], 3)
     for i in range(1, len(adapters)):
-        connections.append(0)
-        for j in range(i-3, i):
-            if j >= 0 and adapters[i] - adapters[j] <= 3:
-                connections[i] += connections[j]
+        connections.extend([0] * (adapters[i] - adapters[i-1] - 1))
+        connections.append(sum(connections))
     return connections[-1]
 
 if __name__ == "__main__":
     with open('day10/input') as f:
         adapters = sorted(map(int, f))
-        adapters.insert(0, 0)
-        adapters.append(adapters[-1] + 3)
+        adapters = [0] + adapters + [adapters[-1] + 3]
 
-        dist = distribution(adapters)
+        dist = Counter(adapters[i] - adapters[i-1] for i in range(1, len(adapters)))
         print(f'Part 1: {dist[1] * dist[3]}')
         print(f'Part 2: {count_arrangements(adapters)}')
