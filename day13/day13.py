cd3fc13771921b0224feb4731c157a5962845aff
Lennart Weckeck <lennart.weckeck@mailbox.org>
2020-12-13 19:33:38 +0100

    Day 13 v2

--- day13/day13.py
+++ day13/day13.py
@@ -4,16 +4,16 @@ import math
 if __name__ == "__main__":
     with open('day13/input') as f:
         earliest_departure = int(f.readline())
-        schedule = [(int(id) if id.isdigit() else 0) for id in f.readline().strip().split(',')]
-        next_departure, id = next((departure, id) for departure in itertools.count(earliest_departure) for id in schedule if id > 0 if departure % id == 0)
-        print(f'Part 1: {(next_departure - earliest_departure) * id}')
+        schedule = [(index, int(interval)) for (index, interval) in enumerate(f.readline().strip().split(',')) if interval != 'x']
+
+        next_departure = min(((interval - (earliest_departure % interval), interval) for (_, interval) in schedule), key=lambda p: p[0])
+        print(f'Part 1: {math.prod(next_departure)}')
 
         # Approach: Chinese remainder theorem sieve
         # (a, n) s.t. x % n == a, where x is the contest solution
-        constraints = [((interval - offset) % interval, interval) for (offset, interval) in enumerate(schedule) if interval != 0]
-        sorted_constraints = sorted(constraints, key=lambda p: p[1], reverse=True) # begin with larger intervals
-        offset, interval = sorted_constraints[0]
-        for next_offset, next_interval in sorted_constraints[1:]:
+        constraints = [((interval - offset) % interval, interval) for (offset, interval) in sorted(schedule, key=lambda p: p[1], reverse=True)]
+        offset, interval = constraints[0]
+        for next_offset, next_interval in constraints[1:]:
             offset = next(x for x in itertools.count(offset, interval) if x % next_interval == next_offset)
-            interval = interval * next_interval // math.gcd(interval, next_interval)
+            interval = interval * next_interval // math.gcd(interval, next_interval) # could be omitted since intervals seem to be pairwise coprimes
         print(f'Part 2: {offset}')
