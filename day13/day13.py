77748a0d05456bf0eeccf183964c0cbc97e56572
Lennart Weckeck <lennart.weckeck@mailbox.org>
2020-12-13 11:53:14 +0100

    Day 13

--- day13/day13.py
+++ day13/day13.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
         next_departure, id = next((departure, id) for departure in itertools.count(earliest_departure) for id in schedule if id > 0 if departure % id == 0)
         print(f'Part 1: {(next_departure - earliest_departure) * id}')
 
+        # Approach: Chinese remainder theorem sieve
         # (a, n) s.t. x % n == a, where x is the contest solution
         constraints = [((interval - offset) % interval, interval) for (offset, interval) in enumerate(schedule) if interval != 0]
         sorted_constraints = sorted(constraints, key=lambda p: p[1], reverse=True) # begin with larger intervals
         offset, interval = sorted_constraints[0]
-        x = 0
         for next_offset, next_interval in sorted_constraints[1:]:
-            x = next(x for x in itertools.count(offset, interval) if x % next_interval == next_offset)
-            offset = x # start with next x, eventual solution cannot be smaller
+            offset = next(x for x in itertools.count(offset, interval) if x % next_interval == next_offset)
             interval = interval * next_interval // math.gcd(interval, next_interval)
-        print(f'Part 2: {x}')
+        print(f'Part 2: {offset}')
